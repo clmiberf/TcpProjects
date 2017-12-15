218b15773a763dcf1e79cf591bbf64dd06773b33
solar <clmiber@outlook.com>
2017-12-15 11:16:35 +0800

    branch test,add a print

--- tcpServer.c
+++ tcpServer.c
@@ -3,7 +3,9 @@
 int main(int argc, char const *argv[]) {
   /* code */
   printf("ÌÆÆ½×îË§\n");
-  printf("If you want to do something,trying your best!");
+  printf("If you want to do something,trying your best!\n");
+  printf("my stupid boss still perfers SVN \n");
+  printf("today is beast\n");
 
   return 0;
 }
